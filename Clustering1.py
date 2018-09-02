c8be08cf2d02e2dbdad3599ea90785729dbe1d00
Jaisimha Manipatruni <40019877+jaisimhamanipatruni@users.noreply.github.com>
2018-09-02 19:32:45 +0530

    Create Clustering1.py

---
+++ Clustering1.py
@@ -0,0 +1,16 @@
+# K-Means-Clustering
+K-Means Clustering
+from sklearn.cluster import KMeans
+
+# Number of clusters
+kmeans = KMeans(n_clusters=3)
+# Fitting the input data
+kmeans = kmeans.fit(X)
+# Getting the cluster labels
+labels = kmeans.predict(X)
+# Centroid values
+centroids = kmeans.cluster_centers_
+
+# Comparing with scikit-learn centroids
+print(C) # From Scratch
+print(centroids) # From sci-kit learn
